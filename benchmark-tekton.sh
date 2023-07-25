1f227ca200db62e7fc9f5194bae3273563a6880e
Khurram Baig <kbaig@redhat.com>
2023-07-25 17:26:39 +0530

    Change to Gnu Parallel for concurrency
    Also added run.yaml and pipeline.yaml. Change option from job to run

--- benchmark-tekton.sh
+++ benchmark-tekton.sh
@@ -6,7 +6,7 @@ long=total:,concurrent:,job:,jsonpath:,help
 
 total=10000
 concurrent=100
-job="https://raw.githubusercontent.com/tektoncd/pipeline/main/examples/v1/pipelineruns/using_context_variables.yaml"
+run="./run.yaml"
 
 read -r -d '' usage <<EOF
 Script needs following options
@@ -16,7 +16,7 @@ Script needs following options
 --total          optional,
                  default value is 10000
 
---job            optional default value is
+--run            optional default value is
                  https://raw.githubusercontent.com/tektoncd/pipeline/main/examples/v1/pipelineruns/using_context_variables.yaml
 
 EOF
@@ -28,7 +28,7 @@ while :; do
     case "${1}" in
         -t | --total        )   total=$2;                                              shift 2                          ;;
         -b | --concurrent   )   concurrent=$2;                                         shift 2                          ;;
-        -s | --job          )   job=$2;                                                shift 2                          ;;
+        -s | --run          )   run=$2;                                                shift 2                          ;;
         --help              )   echo "${usage}" 1>&2;                                  exit                             ;;
         --                  )   shift;                                                 break                            ;;
         *                   )   echo "Error parsing, incorrect options ${format}";     exit 1                           ;;
@@ -39,21 +39,24 @@ done
 while [ "$total" -ne 0 ]
 do
     running=$(kubectl get pr -o=jsonpath='{.items[?(@.status.conditions[0].status=="Unknown")].metadata.name}' | wc -w)
-    all=$(expr  $(kubectl get pr | wc --line) - 1)
+    all=$(expr $(kubectl get pr | wc -l) - 1)
+    scheduled=$(kubectl get pr -o=jsonpath='{.items[?(@.status.conditions[0].type=="Succeeded")].metadata.name}' | wc -w)
+    running=$(expr $all - $scheduled + $running)
 
     if [ "$all" -ge "$total" ]; then
         break
     fi
 
+    echo "running $running"
+    echo "all $all"
+    echo "scheduled $scheduled"
+
     if [ "$running" -lt "$concurrent" ]; then
-        run=$(expr $concurrent - $running)
-        for i in {1..$run}; do
-            kubectl create -f $job &
-        done
+        req=$(expr $concurrent - $running)
+        echo "running ${req} runs to get back to $concurrent level"
+        parallel -N0 kubectl create -f $run >/dev/null 2>&1 ::: $(seq 1 ${req})
+        kubectl delete pod --field-selector=status.phase==Succeeded
     fi
-    wait
 done
 
-echo $total
-echo $concurrent
-echo $job
+echo "done with $total runs of $run which ran with $concurrent runs"
