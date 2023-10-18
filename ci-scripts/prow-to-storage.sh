ffbf2f2e4d52579843c0bd256f72e4f7a4909d7d
Jan Hutar <jhutar@redhat.com>
2023-10-18 20:59:17 +0200

    fix: Finish TODOs now when we have final run

--- ci-scripts/prow-to-storage.sh
+++ ci-scripts/prow-to-storage.sh
@@ -2,7 +2,7 @@
 
 source $(dirname $0)/lib.sh
 
-JOB_BASE="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/origin-ci-test/logs/TODO"
+JOB_BASE="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/origin-ci-test/logs/periodic-ci-openshift-pipelines-performance-master-scaling-pipelines-daily"
 CACHE_DIR="prow-to-es-cache-dir"
 ES_HOST="http://elasticsearch.intlab.perf-infra.lab.eng.rdu2.redhat.com"
 ES_INDEX="pipelines_ci_status_data"
@@ -127,9 +127,11 @@ function upload_horreum() {
         -d "@$f"
 }
 
+counter=0
+
 # Fetch JSON files from main test that runs every 12 hours
 for i in $(curl -SsL "$JOB_BASE" | grep -Eo '[0-9]{19}' | sort -V | uniq | tail -n 10); do
-    f="$JOB_BASE/$i/artifacts/TODO/TODO/artifacts/benchmark-tekton.json"
+    f="$JOB_BASE/$i/artifacts/scaling-pipelines-daily/openshift-pipelines-scaling-pipelines/artifacts/benchmark-tekton.json"
     out="$CACHE_DIR/$i.benchmark-tekton.json"
 
     download "$f" "$out"
@@ -137,4 +139,7 @@ for i in $(curl -SsL "$JOB_BASE" | grep -Eo '[0-9]{19}' | sort -V | uniq | tail 
     upload_basic "$out" "$i"
     enritch_stuff "$out" '."$schema"' "urn:openshift-pipelines-perfscale-scalingPipelines:0.1"
     upload_horreum "$out" "openshift-pipelines-perfscale-scalingPipelines" ".metadata.env.BUILD_ID" "$i"
+    let counter+=1
 done
+
+info "Processed $counter files"
