8446d154f7c11231f76f9ea5690ed47f32b023aa
Jan Hutar <jhutar@redhat.com>
2023-10-30 09:33:37 +0100

    feat: Collect raw data as well

--- ci-scripts/collect-results.sh
+++ ci-scripts/collect-results.sh
@@ -9,9 +9,11 @@ source "$(dirname "$0")/lib.sh"
 ARTIFACT_DIR=${ARTIFACT_DIR:-artifacts}
 monitoring_collection_data=$ARTIFACT_DIR/benchmark-tekton.json
 monitoring_collection_log=$ARTIFACT_DIR/monitoring-collection.log
+monitoring_collection_dir=$ARTIFACT_DIR/monitoring-collection-raw-data-dir
 
 info "Collecting artifacts..."
 mkdir -p "${ARTIFACT_DIR}"
+mkdir -p "${monitoring_collection_dir}"
 [ -f tests/scaling-pipelines/benchmark-tekton.json ] && cp tests/scaling-pipelines/benchmark-tekton.json "${ARTIFACT_DIR}/"
 [ -f tests/scaling-pipelines/benchmark-tekton-runs.json ] && cp tests/scaling-pipelines/benchmark-tekton-runs.json "${ARTIFACT_DIR}/"
 
@@ -39,6 +41,7 @@ if [ -f "$monitoring_collection_data" ]; then
         --additional ./config/cluster_read_config.yaml \
         --monitoring-start "$mstart" \
         --monitoring-end "$mend" \
+        --monitoring-raw-data-dir "$monitoring_collection_dir" \
         --prometheus-host "https://$mhost" \
         --prometheus-port 443 \
         --prometheus-token "$(oc whoami -t)" \
