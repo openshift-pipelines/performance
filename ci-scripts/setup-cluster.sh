4263c0efadd402eb3749e0cbca0e912d199f4d25
Jan Hutar <jhutar@redhat.com>
2023-10-30 16:17:29 +0100

    fix: Do not fail if valiudation fails

--- ci-scripts/setup-cluster.sh
+++ ci-scripts/setup-cluster.sh
@@ -101,11 +101,11 @@ elif [ "$DEPLOYMENT_TYPE" == "upstream" ]; then
     if [ "$DEPLOYMENT_VERSION" == "stable" ]; then
         curl https://storage.googleapis.com/tekton-releases/pipeline/latest/release.notags.yaml \
             | yq 'del(.spec.template.spec.containers[].securityContext.runAsUser, .spec.template.spec.containers[].securityContext.runAsGroup)' \
-            | kubectl apply -f -
+            | kubectl apply --validate=warn -f -
     elif [ "$DEPLOYMENT_VERSION" == "nightly" ]; then
         curl https://storage.googleapis.com/tekton-releases-nightly/pipeline/latest/release.notags.yaml \
             | yq 'del(.spec.template.spec.containers[].securityContext.runAsUser, .spec.template.spec.containers[].securityContext.runAsGroup)' \
-            | kubectl apply -f -
+            | kubectl apply --validate=warn -f -
     else
         fatal "Unknown deployment version '$DEPLOYMENT_VERSION'"
     fi
