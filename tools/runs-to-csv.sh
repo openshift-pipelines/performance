f0d3bd62060d9ec01670b00197c309c5ca3b74bf
Jan Hutar <jhutar@redhat.com>
2024-01-16 09:25:29 +0100

    feat: Create helper script to output CSV file based on all found benchmark-tekton.json files

---
+++ tools/runs-to-csv.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# Just a helper script to output CSV file based on all found benchmark-tekton.json files
+
+find . -name benchmark-tekton.json -print0 | while IFS= read -r -d '' filename; do 
+    cat "${filename}" | jq --raw-output '[
+        .metadata.env.BUILD_ID,
+        .results.started,
+        .results.ended,
+        .parameters.test.run,
+        .parameters.test.total,
+        .parameters.test.concurrent,
+        .results.PipelineRuns.count.succeeded,
+        .results.PipelineRuns.count.failed,
+        .results.PipelineRuns.pending.avg,
+        .results.PipelineRuns.running.avg,
+        .results.PipelineRuns.duration.avg,
+        .results.TaskRuns.pending.avg,
+        .results.TaskRuns.running.avg,
+        .results.TaskRuns.duration.avg,
+        .results.TaskRuns_to_Pods.creationTimestamp_diff.mean,
+        .measurements."tekton-pipelines-controller".count_ready.mean,
+        .measurements."tekton-pipelines-controller".cpu.mean,
+        .measurements."tekton-pipelines-controller".memory.mean,
+        .measurements."tekton-pipelines-webhook".count_ready.mean,
+        .measurements."tekton-pipelines-webhook".cpu.mean,
+        .measurements."tekton-pipelines-webhook".memory.mean,
+        .measurements.tekton_tekton_pipelines_controller_workqueue_depth.mean,
+        .measurements.tekton_pipelines_controller_running_taskruns_throttled_by_node.mean,
+        .measurements.tekton_pipelines_controller_running_taskruns_throttled_by_quota.mean,
+        .measurements.tekton_pipelines_controller_client_latency_average.mean,
+        .measurements.tekton_pipelines_controller_taskruns_pod_latency_milliseconds.mean,
+        .measurements.etcd_request_duration_seconds_average.mean,
+        .measurements.cluster_cpu_usage_seconds_total_rate.mean,
+        .measurements.cluster_memory_usage_rss_total.mean,
+        .measurements.scheduler_pending_pods_count.mean
+    ] | @csv'
+done
