7759e1a642438a3fce7781f584305cfabb045206
Jan Hutar <jhutar@redhat.com>
2024-01-19 11:51:51 +0100

    feat: Add tool to generate graphs from measure-signed.py csv

---
+++ tools/generate-measure-signed-avg.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Computes average of many given measure-signed.csv, per column and row
+
+import sys
+import csv
+
+
+output_file = sys.argv[-1]   # last param is output file name
+data = []
+keys = ["all", "succeeded", "signed", "unsigned", "guessed avg", "guessed from count"]
+
+for file in sys.argv[1:-1]:   # first param is script name, last is output file name
+    print(f"Loading file {file}")
+    with open(file, "r") as fd:
+        for i, row in enumerate(csv.DictReader(fd)):
+            try:
+                current = data[i]
+            except IndexError:
+                current = {
+                    "sums": {k: 0.0 for k in keys},
+                    "counts": {k: 0 for k in keys},
+                }
+                data.append(current)
+
+            for key in keys:
+                current["sums"][key] += float(row[key])
+                current["counts"][key] += 1
+
+print(f"Saving summary to {output_file}")
+with open(output_file, "w") as fd:
+    csv_writer = csv.DictWriter(fd, ["date"] + keys)
+    csv_writer.writeheader()
+    for item in data:
+        row = {"date": None}
+        for key in keys:
+            if item["counts"][key] == 0:
+                row[key] = 0
+            else:
+                row[key] = item["sums"][key] / item["counts"][key]
+        csv_writer.writerow(row)
---
+++ tools/generate-measure-signed.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# Looks for direcotries "monitoring-collection-raw-data-dir" and CSV
+# files in these. Then generates HTML document with graphs, one graph
+# for all files of a given name.
+#
+# First parameter is direcotry where to look for
+# "monitoring-collection-raw-data-dir" direcotries.
+#
+# Second parameter is a direcotry where to put the output.
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+source "$(dirname "$0")/../ci-scripts/lib.sh"
+
+output_dir="signatures/"
+mkdir -p "$output_dir/graphs/"
+columns_to_names="
+2 all
+3 succeeded
+4 signed
+5 unsigned
+6 guessed avg
+"
+myoutput="$output_dir/dashboard.html"
+
+function script_base() {
+    local title="$1"
+    local output_relative="$2"
+    echo -n "
+        set datafile sep ','
+        set key autotitle columnhead   # basically just to skip first line
+        set style data lines
+        set title '$title'
+        set terminal pngcairo linewidth 2
+        set key noenhanced   # do not interpret underscores as text formatting
+        set title noenhanced
+        set output '$output_dir/$output_relative'
+        plot"
+}
+function html_img() {
+    local title="$1"
+    local output_relative="$2"
+    echo "<img src='$output_relative' alt='$title' width='500'/>" >>"$myoutput"
+}
+
+echo "<html><head><title>Generated $( date --utc -Ins )</title></head><body>" >"$myoutput"
+
+for path_grepper in "run-1000-20" "run-1000-40" "run-1000-60" "run-1000-80" "run-1000-100"; do
+    info "Working with path grepper $path_grepper"
+    echo "<h1>Graphs for $path_grepper</h1>" >>"$myoutput"
+    data_files=$( find . -type f -name measure-signed.csv | grep "$path_grepper" )
+    path_title_part=$( echo "$path_grepper" | sed 's/[^a-zA-Z0-9]/_/g' )
+    for column in 2 3 4 6; do
+        column_name=$( echo "$columns_to_names" | grep "^$column " | sed 's/^[0-9]\+ //' )
+        column_title_part=$( echo "$column_name" | sed 's/[^a-zA-Z0-9]/_/g' )
+        output_file="graphs/graphs-$path_title_part-$column_title_part.png"
+        script="$( script_base "$path_grepper - $column_name" "$output_file" )"
+        for f in $data_files; do
+            title=$( echo "$f" | cut -d '/' -f 2 )
+            script="$script '$f' using $column title '$title',"
+        done
+
+        echo -e "$script" | gnuplot
+
+       html_img "Graph for $path_grepper - $column_name" "$output_file" >>"$myoutput"
+    done
+
+    info "Creating measure-signed graphs"
+    ../../tools/generate-measure-signed-avg.py $( find . -type f -name measure-signed.csv | grep "$path_grepper" ) "$output_dir/measure-signed-$path_title_part.csv"
+    output_file="graphs/graphs-$path_title_part-measure-signed.png"
+    script="$( script_base "$path_grepper - measure-signed" "$output_file" )"
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 2,"
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 3,"
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 4"
+    echo -e "$script" | gnuplot
+    html_img "Graph for $path_grepper - measure-signed" "$output_file" >>"$myoutput"
+
+    output_file="graphs/graphs-$path_title_part-measure-signed-guessed-avg.png"
+    script="$( script_base "$path_grepper - measure-signed guessed avg" "$output_file" )"
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 6"
+    echo -e "$script" | gnuplot
+    html_img "Graph for $path_grepper - measure-signed guessed avg" "$output_file" >>"$myoutput"
+done
+
+info "Working on summary"
+echo "<h1>Summary</h1>" >>"$myoutput"
+output_file="graphs/graphs-summary-measure-signed-all.png"
+script="$( script_base "summary - measure-signed all" "$output_file" )"
+for path_grepper in "run-1000-20" "run-1000-40" "run-1000-60" "run-1000-80" "run-1000-100"; do
+    path_title_part=$( echo "$path_grepper" | sed 's/[^a-zA-Z0-9]/_/g' )
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 2 title '$path_grepper',"
+done
+echo -e "$script" | gnuplot
+html_img "Graph for summary - measure-signed all" "$output_file" >>"$myoutput"
+
+output_file="graphs/graphs-summary-measure-signed-succeeded.png"
+script="$( script_base "summary - measure-signed succeeded" "$output_file" )"
+for path_grepper in "run-1000-20" "run-1000-40" "run-1000-60" "run-1000-80" "run-1000-100"; do
+    path_title_part=$( echo "$path_grepper" | sed 's/[^a-zA-Z0-9]/_/g' )
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 3 title '$path_grepper',"
+done
+echo -e "$script" | gnuplot
+html_img "Graph for summary - measure-signed succeeded" "$output_file" >>"$myoutput"
+
+output_file="graphs/graphs-summary-measure-signed-signed.png"
+script="$( script_base "summary - measure-signed signed" "$output_file" )"
+for path_grepper in "run-1000-20" "run-1000-40" "run-1000-60" "run-1000-80" "run-1000-100"; do
+    path_title_part=$( echo "$path_grepper" | sed 's/[^a-zA-Z0-9]/_/g' )
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 4 title '$path_grepper',"
+done
+echo -e "$script" | gnuplot
+html_img "Graph for summary - measure-signed signed" "$output_file" >>"$myoutput"
+
+output_file="graphs/graphs-summary-measure-signed-guessed-avg.png"
+script="$( script_base "summary - measure-signed guessed avg" "$output_file" )"
+for path_grepper in "run-1000-20" "run-1000-40" "run-1000-60" "run-1000-80" "run-1000-100"; do
+    path_title_part=$( echo "$path_grepper" | sed 's/[^a-zA-Z0-9]/_/g' )
+    script="$script '$output_dir/measure-signed-$path_title_part.csv' using 6 title '$path_grepper',"
+done
+echo -e "$script" | gnuplot
+html_img "Graph for summary - measure-signed guessed avg" "$output_file" >>"$myoutput"
+
+echo "</body></html>" >>"$myoutput"
+
+info "Dumped dashboard to $myoutput"
