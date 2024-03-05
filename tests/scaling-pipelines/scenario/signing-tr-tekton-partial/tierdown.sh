0d5e6e3f2b95ad80883711d5644c4baa90b4ff14
Jan Hutar <jhutar@redhat.com>
2024-03-05 12:11:37 +0100

    fix: This script is waiting for TRs, not PRs

--- tests/scaling-pipelines/scenario/signing-tr-tekton-partial/tierdown.sh
+++ tests/scaling-pipelines/scenario/signing-tr-tekton-partial/tierdown.sh
@@ -6,7 +6,8 @@ set_started_now
 
 # Now we will create second half PRs
 # Unfortunately this overrides stats in JSON file
-generate_more_start "${TEST_TOTAL_ORIG}" "${TEST_CONCURRENT}" "${TEST_RUN}" "${TEST_TIMEOUT:-18000}"
+# Note each PR have 3 TRs
+generate_more_start "$(( TEST_TOTAL_ORIG * 3 ))" "${TEST_CONCURRENT}" "${TEST_RUN}" "${TEST_TIMEOUT:-18000}"
 
 generate_more_wait
 
