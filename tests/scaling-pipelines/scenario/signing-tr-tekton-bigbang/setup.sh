adbce2b2e436819bab9fbb774dce1806f05f9681
Jan Hutar <jhutar@redhat.com>
2024-03-25 15:06:30 +0100

    feat: Attempt to run the bigbang test in a way that it will run benchmark.py only once

--- tests/scaling-pipelines/scenario/signing-tr-tekton-bigbang/setup.sh
+++ tests/scaling-pipelines/scenario/signing-tr-tekton-bigbang/setup.sh
@@ -3,3 +3,11 @@ source scenario/common/lib.sh
 chains_setup_tekton_tekton_
 
 chains_stop
+
+(
+    wait_for_prs_finished "${TEST_TOTAL}"
+    chains_start
+    set_started_now
+) &
+
+export TEST_PARAMS="--wait-for-state signed_true"
--- tests/scaling-pipelines/scenario/signing-tr-tekton-bigbang/tierdown.sh
+++ tests/scaling-pipelines/scenario/signing-tr-tekton-bigbang/tierdown.sh
@@ -1,13 +1,3 @@
 source scenario/common/lib.sh
 
-measure_signed_start
-
-chains_start
-
-set_started_now
-
-measure_signed_wait
-
 set_ended_now
-
-measure_signed_stop
