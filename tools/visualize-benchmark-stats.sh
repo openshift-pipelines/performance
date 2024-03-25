91da4ba6f81fdce72b19ba2ee36400432a6660cd
Jan Hutar <jhutar@redhat.com>
2024-03-25 10:57:59 +0100

    feat: Stub of a tool to visualize benchmark-stats.sh data

---
+++ tools/visualize-benchmark-stats.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+data_file="${1}"
+
+{
+    echo -n "
+        set datafile sep ','
+        set key autotitle columnhead   # basically just to skip first line which contains header
+        set xdata time
+        set timefmt '%Y-%m-%dT%H:%M:%S.000000+00:00'
+        set style data lines
+        set title 'PipelineRuns $data_file'
+        set terminal pngcairo linewidth 1
+        set key noenhanced autotitle columnhead  # do not interpret underscores as text formatting
+        set title noenhanced
+        set xlabel 'Time'
+        set ylabel 'Count'
+        set output 'benchmark-stats-pipelineruns.png'
+        plot"
+    echo -n " '$data_file' using 2:4 title 'prs_total' linewidth 2,"
+    echo -n " '$data_file' using 2:5 title 'prs_pending' linewidth 2,"
+    echo -n " '$data_file' using 2:6 title 'prs_running' linewidth 2,"
+    echo -n " '$data_file' using 2:7 title 'prs_finished' linewidth 2,"
+    echo -n " '$data_file' using 2:8 title 'prs_signed_true' linewidth 2,"
+    echo -n " '$data_file' using 2:10 title 'prs_finalizers_present' linewidth 2,"
+} | gnuplot
+
+{
+    echo -n "
+        set datafile sep ','
+        set key autotitle columnhead   # basically just to skip first line which contains header
+        set xdata time
+        set timefmt '%Y-%m-%dT%H:%M:%S.000000+00:00'
+        set style data lines
+        set title 'TaskRuns $data_file'
+        set terminal pngcairo linewidth 1
+        set key noenhanced autotitle columnhead  # do not interpret underscores as text formatting
+        set title noenhanced
+        set xlabel 'Time'
+        set ylabel 'Count'
+        set output 'benchmark-stats-taskrunsruns.png'
+        plot"
+    echo -n " '$data_file' using 2:14 title 'trs_total' linewidth 2,"
+    echo -n " '$data_file' using 2:15 title 'trs_pending' linewidth 2,"
+    echo -n " '$data_file' using 2:16 title 'trs_running' linewidth 2,"
+    echo -n " '$data_file' using 2:17 title 'trs_finished' linewidth 2,"
+    echo -n " '$data_file' using 2:18 title 'trs_signed_true' linewidth 2,"
+    echo -n " '$data_file' using 2:20 title 'trs_finalizers_present' linewidth 2,"
+} | gnuplot
