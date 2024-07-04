7c2755f5af12745dddc08e74d7b9747663457067
Rahul Shetty <rashetty@redhat.com>
2024-07-04 15:50:35 +0530

    fixed error handling for 0 success scenario + added new resolutionrequests metric to collector script

--- tools/stats-resolutionrequests.sh
+++ tools/stats-resolutionrequests.sh
@@ -19,7 +19,7 @@ object_lists=$(echo "$items" | jq '. += {"apiVersion":"v1", "kind": "List", "met
 # Filter run details based on outcome 
 data_overall=$(echo "$object_jsons" | jq --raw-output '.items |= [.[] | . as $a | if . == null then [] else . end | $a ]')
 data_successful=$(echo "$object_jsons" | jq --raw-output '.items |= [.[] | . as $a | if . == null then [] else . end | select(.status.conditions[0].type == "Succeeded" and .status.conditions[0].status == "True") | $a ]')
-data_failed=$(echo "$object_jsons" | jq --raw-output '.items |= [.[] | . as $a | if . == null then [] else . end | select(.status.conditions[0].type != "Succeeded" and .status.conditions[0].status != "True") | $a ]')
+data_failed=$(echo "$object_jsons" | jq --raw-output '.items |= [.[] | . as $a | if . == null then [] else . end | select(.status.conditions[0].status == "False") | $a ]')
 
 # In case the test doesn't contain ResolutionRequest, then terminate.
 req_overall=$(echo "$data_overall" | jq --raw-output '[.items[]] | length')
@@ -35,10 +35,15 @@ req_max=$(echo "$data_overall" | jq --raw-output '[.items[] | ((.status.conditio
 cat $output | jq '.results.ResolutionRequests.Overall.duration.min = '$req_min' | .results.ResolutionRequests.Overall.duration.avg = '$req_avg' | .results.ResolutionRequests.Overall.duration.max = '$req_max'' >"$$.json" && mv -f "$$.json" "$output"
 
 # [Success] ResolutionRequest duration (.status.conditions[0].lastTransitionTime - .metadata.creationTimestamp)
-req_avg=$(echo "$data_successful" | jq --raw-output '[.items[] | ((.status.conditions[0].lastTransitionTime | fromdate) - (.metadata.creationTimestamp | fromdate))] | add / length')
-req_min=$(echo "$data_successful" | jq --raw-output '[.items[] | ((.status.conditions[0].lastTransitionTime | fromdate) - (.metadata.creationTimestamp | fromdate))] | min')
-req_max=$(echo "$data_successful" | jq --raw-output '[.items[] | ((.status.conditions[0].lastTransitionTime | fromdate) - (.metadata.creationTimestamp | fromdate))] | max')
-cat $output | jq '.results.ResolutionRequests.Success.duration.min = '$req_min' | .results.ResolutionRequests.Success.duration.avg = '$req_avg' | .results.ResolutionRequests.Success.duration.max = '$req_max'' >"$$.json" && mv -f "$$.json" "$output"
+req_success=$(echo "$data_successful" | jq --raw-output '[.items[]] | length')
+if [ "$req_success" != "0" ]; then
+    req_avg=$(echo "$data_successful" | jq --raw-output '[.items[] | ((.status.conditions[0].lastTransitionTime | fromdate) - (.metadata.creationTimestamp | fromdate))] | add / length')
+    req_min=$(echo "$data_successful" | jq --raw-output '[.items[] | ((.status.conditions[0].lastTransitionTime | fromdate) - (.metadata.creationTimestamp | fromdate))] | min')
+    req_max=$(echo "$data_successful" | jq --raw-output '[.items[] | ((.status.conditions[0].lastTransitionTime | fromdate) - (.metadata.creationTimestamp | fromdate))] | max')
+    cat $output | jq '.results.ResolutionRequests.Success.duration.min = '$req_min' | .results.ResolutionRequests.Success.duration.avg = '$req_avg' | .results.ResolutionRequests.Success.duration.max = '$req_max'' >"$$.json" && mv -f "$$.json" "$output"
+else 
+    echo "DEBUG: No success runs found. Skipping success resolution run calculation..."
+fi
 
 # [Failed] ResolutionRequest duration (.status.conditions[0].lastTransitionTime - .metadata.creationTimestamp)
 req_failed=$(echo "$data_failed" | jq --raw-output '[.items[]] | length')
@@ -52,4 +57,5 @@ else
 fi
 
 # Save result list
+echo "DEBUG: ResolutionRequest Total ($req_overall) | Success ($req_success) | Failed ($req_failed)"
 echo "$object_lists" > resolutionrequests.json
