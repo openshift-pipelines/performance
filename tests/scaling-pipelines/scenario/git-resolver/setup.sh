8b08c0213167b4ba6383ad4735959b041649ff7c
siddardh <sira@redhat27!>
2024-08-09 17:10:50 +0530

    Fixed some typo mistakes

--- tests/scaling-pipelines/scenario/git-resolver/setup.sh
+++ tests/scaling-pipelines/scenario/git-resolver/setup.sh
@@ -7,14 +7,14 @@ TEST_NAMESPACE="${TEST_NAMESPACE:-1}"
 TEST_RESOLVER_TYPE="${TEST_RESOLVER_TYPE:-git}"
 
 if [ "$TEST_RESOLVER_TYPE" != "git" ] && [ "$TEST_RESOLVER_TYPE" != "task" ]; then
-    fatal "Invalid TEST_CLUSTER_RESOLVER__TYPE parameter (value: $TEST_RESOLVER_TYPE) set. Possible values: git/task"
+    fatal "Invalid TEST_RESOLVER_TYPE parameter (value: $TEST_RESOLVER_TYPE) set. Possible values: git/task"
 fi
 
 chains_setup_tekton_tekton_
 
 chains_stop
 
-# Create GitResolver's pipeline definition from template based on resolver type (refer to cluster scoped tasks in utils or per namespace tasks using default definition)
+# Create GitResolver's pipeline definition from template based on resolver type
 # This will be applied by load-test.sh,  post to the setup script execution
 create_pipeline_from_j2_template pipeline.yaml.j2 "namespace_count=${TEST_NAMESPACE}, resolver_type=${TEST_RESOLVER_TYPE}"
 
