51f485e9caaf17eb4a5c610e0568033a736cd016
Jan Hutar <jhutar@redhat.com>
2025-05-14 11:29:21 +0200

    feat(SRVKP-7560): Update puller script to use code from Konflux CI puller

--- ci-scripts/prow-to-storage.sh
+++ ci-scripts/prow-to-storage.sh
@@ -2,36 +2,29 @@
 
 source $(dirname $0)/lib.sh
 
-JOB_BASE="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/origin-ci-test/logs/"
+PROW_GCSWEB_HOST="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com"
 CACHE_DIR="prow-to-es-cache-dir"
-ES_HOST="http://elasticsearch.intlab.perf-infra.lab.eng.rdu2.redhat.com"
-ES_INDEX="pipelines_ci_status_data"
 HORREUM_HOST="https://horreum.corp.redhat.com"
-HORREUM_KEYCLOAK_HOST="https://horreum-keycloak.corp.redhat.com"
+DASHBOARD_ES_INDEX="results-dashboard-data"
 
-mkdir -p "$CACHE_DIR"
+DRY_RUN=false
+DRY_RUN=true
 
+# Check for required tools
+if ! type shovel.py &>/dev/null; then
+    fatal "shovel.py utility not available"
+fi
 if ! type jq >/dev/null; then
     fatal "Please install jq"
 fi
-if [ -z "$HORREUM_JHUTAR_PASSWORD" ]; then
-    fatal "Please provide HORREUM_JHUTAR_PASSWORD variable"
+
+# Check for required secrets
+if [ -z "${HORREUM_API_TOKEN:-}" ]; then
+    fatal "Please provide HORREUM_API_TOKEN variable"
 fi
 
-format_date() {
-    date -d "$1" +%FT%TZ --utc
-}
+mkdir -p "$CACHE_DIR"
 
-function download() {
-    local from="$1"
-    local to="$2"
-    if ! [ -f "$to" ]; then
-        info "Downloading $from to $to"
-        curl -LSsl -o "$to" "$from"
-    else
-        debug "File $to already present, skipping download"
-    fi
-}
 
 function check_json() {
     local f="$1"
@@ -40,10 +33,12 @@ function check_json() {
         return 0
     else
         error "File is not a valid JSON, removing it and skipping further processing"
+        head "$f"
         rm -f "$f"
         return 1
     fi
 }
+
 
 function check_json_string() {
     local data="$1"
@@ -54,6 +49,22 @@ function check_json_string() {
         return 1
     fi
 }
+
+
+function json_complete() {
+    local f="$1"
+    local started="$( jq --raw-output .started "$f" )"
+    if [ -z "$started" ] || [ "$started" = "null" ]; then
+        error "File $f does not contain started filed: '$started'"
+        return 1
+    fi
+    local ended="$( jq --raw-output .ended "$f" )"
+    if [ -z "$ended" ] || [ "$ended" = "null" ]; then
+        error "File $f does not contain ended filed: '$ended'"
+        return 1
+    fi
+}
+
 
 function enritch_stuff() {
     local f="$1"
@@ -71,81 +82,97 @@ function enritch_stuff() {
     fi
 }
 
-function upload_basic() {
-    local f="$1"
-    local build_id="$2"
-    local current_doc_in_es=$( curl --silent -X GET $ES_HOST/$ES_INDEX/_search -H 'Content-Type: application/json' -d '{"query":{"term":{"metadata.env.BUILD_ID.keyword":{"value":"'$build_id'"}}}}' | jq --raw-output .hits.total.value )
-    if [[ "$current_doc_in_es" -gt 0 ]]; then
-        info "Already in ES, skipping upload"
-        return 0
-    fi
 
-    info "Uploading to ES"
-    curl --silent \
-        -X POST \
-        -H 'Content-Type: application/json' \
-        $ES_HOST/$ES_INDEX/_doc \
-        -d "@$f" | jq --raw-output .result
+function prow_list() {
+    local job="$1"
+    shovel.py prow --job-name "$job" list
 }
 
-function upload_horreum() {
-    local f="$1"
-    local test_name="$2"
-    local test_matcher="$3"
-    local build_id="$4"
 
-    if [ ! -f "$f" -o -z "$test_name" -o -z "$test_matcher" -o -z "$build_id" ]; then
-        error "Insufficient parameters when uploading to Horreum"
-        return 1
+function prow_subjob_list() {
+    local url="$1"
+    # Note: this `... | rev | cut ... | rev` is just a hack how to get last field
+    shovel.py html links --url "$url" --regexp '.*/run-[^/]+/$' | rev | cut -d '/' -f 1 | rev
+}
+
+
+function prow_download() {
+    local job="$1"
+    local id="$2"
+    local run="$3"
+    local path="$4"
+    local out="$5"
+    if [ -e "$out" ]; then
+        debug "We already have $out, not overwriting it"
+    else
+        shovel.py prow --job-name "$job" download --job-run-id $id --run-name "$run" --artifact-path "$path" --output-path "$out"
+        debug "Downloaded $out"
     fi
+}
 
-    local test_owner="Openshift-pipelines-team"
+
+function horreum_upload() {
+    local f="$1"
+    local test_job_matcher="${2:-jobName}"
+    local test_job_matcher_label="${3:-jobName}"
+
+    local test_owner="rhtap-perf-test-team"
     local test_access="PUBLIC"
 
-    local test_start="$( format_date $( cat "$f" | jq --raw-output ".results.started" ) )"
-    local test_end="$( format_date $( cat "$f" | jq --raw-output ".results.ended" ) )"
+    local test_matcher="$( status_data.py --status-data-file "$f" --get $test_job_matcher )"
 
-    if [ -z "$test_start" -o -z "$test_end" -o "$test_start" == "null" -o "$test_end" == "null" ]; then
-        error "We need start ($test_start) and end ($test_end) time in the JSON we are supposed to upload"
-        return 1
+    debug "Uploading to Horreum: $f with $test_job_matcher_label(a.k.a. $test_job_matcher): $test_matcher"
+
+    if $DRY_RUN; then
+        echo shovel.py horreum --base-url "$HORREUM_HOST" --api-token "..." upload --test-name "@name" --input-file "$f" --matcher-field "$test_job_matcher" --matcher-label "$test_job_matcher_label" --start "@started" --end "@ended" --trashed --trashed-workaround-count 20
+        echo shovel.py horreum --base-url "$HORREUM_HOST" --api-token "..." result --test-name "@name" --output-file "$f" --start "@started" --end "@ended"
+    else
+        shovel.py horreum --base-url "$HORREUM_HOST" --api-token "$HORREUM_API_TOKEN" upload --test-name "@name" --input-file "$f" --matcher-field "$test_job_matcher" --matcher-label "$test_job_matcher_label" --start "@started" --end "@ended" --trashed --trashed-workaround-count 20
+        shovel.py horreum --base-url "$HORREUM_HOST" --api-token "$HORREUM_API_TOKEN" result --test-name "@name" --output-file "$f" --start "@started" --end "@ended"
     fi
-
-    debug "Considering $f to upload to Horreum: start: $test_start, end: $test_end, $test_matcher: $build_id"
-
-    local TOKEN=$( curl -s $HORREUM_KEYCLOAK_HOST/realms/horreum/protocol/openid-connect/token -d "username=jhutar@redhat.com" -d "password=$HORREUM_JHUTAR_PASSWORD" -d "grant_type=password" -d "client_id=horreum-ui" | jq --raw-output .access_token )
-
-    local test_id=$( curl --silent --get -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "$HORREUM_HOST/api/test/byName/$test_name" | jq --raw-output .id )
-
-    local exists=$( curl --silent --get -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "$HORREUM_HOST/api/dataset/list/$test_id" --data-urlencode "filter={\"$test_matcher\":\"$build_id\"}" | jq --raw-output '.datasets | length' )
-
-    if [[ $exists -gt 0 ]]; then
-        info "Test result $f ($test_matcher=$build_id) found in Horreum ($exists), skipping upload"
-        return 0
-    fi
-
-    info "Uploading $f to Horreum"
-    curl --silent \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $TOKEN" \
-        "$HORREUM_HOST/api/run/data?test=$test_name&start=$test_start&stop=$test_end&owner=$test_owner&access=$test_access" \
-        -d "@$f"
 }
+
+
+function resultsdashboard_upload() {
+    local file="$1"
+    local group="$2"
+    local product="$3"
+    local version="$4"
+
+    debug "Uploading to Results Dashboard: $file"
+
+    if $DRY_RUN; then
+        echo shovel.py resultsdashboard --base-url $ES_HOST upload --input-file "$file" --group "$group" --product "$product" --test @name --result-id @metadata.env.BUILD_ID --result @result --date @started --link @jobLink --release latest --version "$version"
+    else
+        shovel.py resultsdashboard --base-url $ES_HOST upload --input-file "$file" --group "$group" --product "$product" --test @name --result-id @metadata.env.BUILD_ID --result @result --date @started --link @jobLink --release latest --version "$version"
+    fi
+}
+
+
+format_date() {
+    date -d "$1" +%FT%TZ --utc
+}
+
 
 counter=0
 
 # Fetch JSON files from main test that runs every 12 hours
+job_path="openshift-pipelines-scaling-pipelines/artifacts/"
+subjob_file="benchmark-tekton.json"
 for job in "scaling-pipelines-upstream-stable-daily" "scaling-pipelines-upstream-nightly-daily"; do
-    job_history="$JOB_BASE/periodic-ci-openshift-pipelines-performance-main-$job"
-    info "Looking into job history: ${job_history}"
-    for i in $(curl -SsL "$job_history" | grep -Eo '[0-9]{19}' | sort -V | uniq | tail -n 10); do
-        f="$job_history/$i/artifacts/$job/openshift-pipelines-scaling-pipelines/artifacts/benchmark-tekton.json"
-        out="$CACHE_DIR/$i.benchmark-tekton.json"
-
-        download "$f" "$out"
+    prow_job="periodic-ci-openshift-pipelines-performance-main-$job"
+    job_base="$PROW_GCSWEB_HOST/gcs/test-platform-results/logs/$prow_job"
+    for i in $( prow_list "$prow_job" ); do
+        ### TODO: We are running more sub-jobs in one job, so need to loop over this:
+        ###prow_subjob_list "$job_base/$i/artifacts/$job/$job_path"
+        out="$CACHE_DIR/$i-$subjob.benchmark-tekton.json"
+        prow_download "$prow_job" "$i" "$job" "$job_path/$subjob/$file" "$out"
         check_json "$out" || continue
-        upload_basic "$out" "$i"
-        enritch_stuff "$out" '."$schema"' "urn:openshift-pipelines-perfscale-scalingPipelines:0.1"
-        upload_horreum "$out" "openshift-pipelines-perfscale-scalingPipelines" ".metadata.env.BUILD_ID" "$i"
+        json_complete "$out" || continue
+        enritch_stuff "$out" "jobLink" "$job_base/$i/artifacts/$job/$job_path/$subjob/$subjob_file"
+        enritch_stuff "$out" "\$schema" "urn:openshift-pipelines-perfscale-scalingPipelines:0.1"
+        horreum_upload "$out" "metadata.env.BUILD_ID" ".metadata.env.BUILD_ID"
+        resultsdashboard_upload "$out" "Developer" "OpenShift Pipelines" "$( date --utc -Idate )"
         let counter+=1
     done
 done
